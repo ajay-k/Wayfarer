8bfb1e1dcfeee50c38e9afa86fb0a953898ea818
ajay-k <ajay_k@sbcglobal.net>
2020-04-22 16:36:16 -0700

    Creates modal, routes

---
+++ wayfarer-app/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from '../config/routes';
+import Navbar from './Navbar.js';
+
+function App() {
+	return(
+		<div>
+		<Navbar />
+		<Routes />
+		</div>
+	);
+}
+
+export default App;
