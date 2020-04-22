f6cccdafd19f0b12a6c2454fddb34899693b2668
ajay-k <ajay_k@sbcglobal.net>
2020-04-22 16:05:30 -0700

    Adds navbar and modal componenets

--- wayfarer-app/src/index.js
+++ wayfarer-app/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  	<Router>
+    	<Navbar />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
