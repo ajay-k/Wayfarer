ca5a886a10e3c0b55c9d63857afd6fd2eb896847
ajay-k <ajay_k@sbcglobal.net>
2020-04-23 09:35:49 -0700

    Adds modal

--- wayfarer-app/src/components/Navbar.js
+++ wayfarer-app/src/components/Navbar.js
@@ -1,35 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom'
+import RegisterModal from './RegisterModal.js'
 
 
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <div className="container">
-        <Link className="navbar-brand"  to="/">Wayfarer</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-​
-        <div className="collapse navbar-collapse" id="navbarsExample04">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" exact to="/">Home</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/profile">Profile</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/register">Register</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/login">Login</NavLink>
-            </li>
-          </ul>
+
+
+class Navbar extends Component {
+  
+ state = {
+    open: false,
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+
+
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand"  to="/">Wayfarer</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+  ​
+          <div className="collapse navbar-collapse" id="navbarsExample04">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" exact to="/">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/profile">Profile</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/register" onClick={this.onOpenModal}>Register</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">Login</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
