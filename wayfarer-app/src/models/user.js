f730e959e97566ed818013347134d27fa2afbab9
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2020-04-23 15:07:09 -0700

    register working

---
+++ wayfarer-app/src/models/user.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const REACT_APP_API_URL = "http://localhost:3001/api/v1"
+
+export default class UserModel {
+    static create(data) {
+        console.log(data)
+        let request = axios.post(`${REACT_APP_API_URL}/auth/register`, data)
+        return request
+    }
+}
